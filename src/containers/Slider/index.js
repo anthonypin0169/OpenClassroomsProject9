3e8640d66945cce108989e4ab3bd4530ccc339ac
Anthony Pin <anthonypin0169@gmail.com>
2026-03-30 18:39:14 +0200

    bug avec les key du slider corrigé

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -22,35 +22,33 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
+      <div
+        key={event.id}
+        className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+      >
+        <img src={event.cover} alt="forum" />
+
+        <div className="SlideCard__descriptionContainer">
+          <div className="SlideCard__description">
+            <h3>{event.title}</h3>
+            <p>{event.description}</p>
+            <div>{getMonth(new Date(event.date))}</div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
+        </div>
+
+        <div className="SlideCard__paginationContainer">
+          <div className="SlideCard__pagination">
+            {byDateDesc.map((currentEvent, radioIdx) => (
+            <input
+              key={currentEvent.id}
+              type="radio"
+              name="radio-button"
+              checked={idx === radioIdx}
+            />
+            ))}
           </div>
-        </>
+        </div>
+      </div>
       ))}
     </div>
   );
